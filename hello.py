9db1a626bb288cf2a4a88e36517893273de64e09
Bronson Naylor <bronsonnaylor@gmail.com>
2019-06-17 03:00:34 -0600

    Updated code

--- hello.py
+++ hello.py
@@ -4,3 +4,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+#new endpoint
+@app.route('/space')
+def to_space():
+    return 'Welcome to space!'
+
+@app.route('/home')
+def my_home():
+    return 'Welcome home.'
